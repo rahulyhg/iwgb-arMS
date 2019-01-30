97fd33f08e094f1ac4d7bd42e62fedfbd1db1633
Guy Mac <hello@guymac.eu>
2019-01-30 12:34:20 +0000

    Add some root schemas

---
+++ src/Action/Public/GenericPublicAction.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hello
+ * Date: 30/01/2019
+ * Time: 12:26
+ */
+
+class GenericPublicAction {
+
+}
---
+++ src/Config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hello
+ * Date: 30/01/2019
+ * Time: 12:22
+ */
+
+class Config {
+
+}
