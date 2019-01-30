bee7667e6efccb1da0bb2f6b485cb6515a553d59
Guy Mac <hello@guymac.eu>
2019-01-30 12:34:55 +0000

    Add public page action

--- src/Action/Public/GenericPublicAction.php
+++ src/Action/Public/GenericPublicAction.php
@@ -1,11 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hello
- * Date: 30/01/2019
- * Time: 12:26
- */
 
-class GenericPublicAction {
+use Slim\Http\Request;
+use Slim\Http\Response;
 
+abstract class GenericPublicAction extends \Action\GenericAction {
+    protected $nav;
+
+    public function __construct($container) {
+        parent::__construct($container);
+        $this->nav = new \JSONObject(\Config::Menus, 'public-top');
+    }
+
+    public function render(Request $request, Response $response, string $template, $vars) {
+        return parent::render($request,$response, $template,
+            array_merge($vars, ['nav' => (array) $this->nav]));
+    }
 }
