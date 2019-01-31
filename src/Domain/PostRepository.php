df35f394a48b248a02df2229cebb9650cc7155a2
Guy Mac <hello@guymac.eu>
2019-01-31 13:13:11 +0000

    Remove dupe code in PostRepository

--- src/Domain/PostRepository.php
+++ src/Domain/PostRepository.php
@@ -25,35 +25,29 @@ class PostRepository extends EntityRepository {
 
     public function getPostsByType(string $type, $where = true, $params = [], $n = self::DEFAULT_LIM) {
         $qb = $this->getEntityManager()->createQueryBuilder();
-        return $this->getPosts($qb->expr()
-            ->andX(
-                $qb->expr()
-                    ->eq('b.type', ':type'),
+        return $this->getPosts($qb->expr()->andX(
+            $this->getEntityManager()
+                ->createQueryBuilder()
+                ->expr()
+                ->eq('b.type', ':type'),
                 $where),
             array_merge($params, ['type' => $type,]),
             $n);
     }
 
     public function getPinnedPost() {
-        $qb = $this->getEntityManager()->createQueryBuilder();
-        $q = $this->getEntityManager()
-            ->createQueryBuilder()
-            ->select('p')
-            ->from('\Domain\Post', 'p')
-            ->innerJoin('p.blog', 'b', Join::WITH)
-            ->where($qb->expr()
-                ->neq('p.headerImage', ':noHeader'))
-            ->orderBy('p.timestamp', 'DESC')
-            ->getQuery()
-            ->setParameter('noHeader', '""')
-            ->setMaxResults(1);
-        return $q->execute();
+        return $this->getPostsByType(\BlogType::Posts,
+            $this->getEntityManager()
+                ->createQueryBuilder()
+                ->expr()
+                ->neq('p.headerImage', ':noHeader'),
+            ['noHeader' => '""'],
+            1);
     }
 
     public function getStoriesExcluding(Post $post, $n = self::DEFAULT_LIM) {
-        $qb = $this->getEntityManager()->createQueryBuilder();
         return $this->getPostsByType(\BlogType::Posts,
-            $qb->expr()
+            $this->getEntityManager()->createQueryBuilder()->expr()
                 ->neq('p.id', ':exclude'),
             ['exclude' => $post->getId()],
             $n);
