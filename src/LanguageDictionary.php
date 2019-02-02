8c513a2b7a22ecdf5b36fd02bb3337c7ef49fc84
Guy Mac <hello@guymac.eu>
2019-02-02 00:51:24 +0000

    phpdoc LanguageDictionary

--- src/LanguageDictionary.php
+++ src/LanguageDictionary.php
@@ -8,6 +8,14 @@ class LanguageDictionary {
         $this->dictionary = \JSONObject::getAll(\Config::Dictionary);
     }
 
+    /**
+     * If no translation is available, $s will be returned.
+     * This method logs unsuccessful translation attempts as empty dictionary entries.
+     *
+     * @param $s string The string to be translated, in the fallback language.
+     * @param $lang string The two-character language identifier of the target language.
+     * @return string If possible, the translated string $s.
+     */
     public function get($s, $lang): string {
         if (!empty($this->dictionary[$s])) {
             if (!empty($this->dictionary[$s][$lang])) {
@@ -22,7 +30,9 @@ class LanguageDictionary {
         }
     }
 
-
+    /**
+     * Write any changes to the in-memory $dictionary to dictionary.json
+     */
     private function flush(): void {
         file_put_contents(APP_ROOT . '/config/lang/dictionary.json',
             json_encode($this->dictionary));
