857eaf50db3a891c2a9578c7c9eab62791e463ab
Guy Mac <hello@guymac.eu>
2019-02-22 22:48:42 +0000

    Add generic logged in member action

--- src/Action/Frontend/GenericMemberAction.php
+++ src/Action/Frontend/GenericMemberAction.php
@@ -2,15 +2,27 @@
 
 namespace Action\Frontend;
 
-abstract class GenericMemberAction extends GenericPublicAction {
+use Action\GenericAction;
+use Slim\Container;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Psr\Http\Message\ResponseInterface;
+
+abstract class GenericMemberAction extends GenericAction {
+
+    protected $member;
+
+    public function __construct(Container $c) {
+        parent::__construct($c);
+        $this->member = $this->em
+            ->getRepository(\Domain\Member::class)
+            ->find($this->session->get('user'));
+    }
 
     /**
-     * @param string $memberId
-     * @return \Domain\Member
+     * {@inheritdoc}
      */
-    protected function getMember(string $memberId): \Domain\Member {
-        /** @var $member \Domain\Member */
-        $member = $this->em->getRepository(\Domain\Member::class)->find($memberId);
-        return $member;
+    public function render(Request $request, Response $response, string $template, $vars): ResponseInterface {
+        return parent::render($request, $response, $template, array_merge(['user' => $this->member], $vars));
     }
 }
