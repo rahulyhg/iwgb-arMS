c441627beccdb8449382c5a9f3090de91e84d3bf
Guy Mac <hello@guymac.eu>
2019-02-25 23:30:58 +0000

    Fixes #6

--- src/Domain/MemberRepository.php
+++ src/Domain/MemberRepository.php
@@ -3,10 +3,56 @@
 namespace Domain;
 
 use Doctrine\ORM\EntityRepository;
+use Exception;
 
 class MemberRepository extends EntityRepository {
 
     const DEFAULT_LIMIT = 20;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function find($id, $lockMode = null, $lockVersion = null, $unconfirmed = false) {
+        try {
+            /** @var Member $member */
+            $member = parent::find($id, $lockMode, $lockVersion);
+        } catch (Exception $e) {
+            return null;
+        }
+        if (!$unconfirmed &&
+            !$member->isConfirmed()) {
+            return null;
+        }
+        return $member;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null, $unconfirmed = false) {
+        try {
+            $members = parent::findBy($criteria, $orderBy, $limit, $offset);
+        } catch (Exception $e) {
+            return null;
+        }
+
+        $results = [];
+
+        if (!empty($members)) {
+            foreach ($members as $member) {
+                /** @var Member $member */
+                if ($unconfirmed &&
+                    !$member->isConfirmed()) {
+                    continue;
+                }
+                $results[] = $member;
+            }
+        } else {
+            return null;
+        }
+        return $results;
+
+    }
 
     public function getMembers($branch = null, int $page = 0, int $n = self::DEFAULT_LIMIT) {
 
@@ -43,6 +89,8 @@ class MemberRepository extends EntityRepository {
             ->setMaxResults($n)
         )->execute();
     }
+
+
 
 
 
