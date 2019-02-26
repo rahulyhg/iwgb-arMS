dd4e4a4fd8ed61ac43124ebac97b344afd968ae5
Guy Mac <hello@guymac.eu>
2019-02-26 19:05:53 +0000

    Fixes #9
    Still requires copy

--- src/Action/Backend/Member/Confirm.php
+++ src/Action/Backend/Member/Confirm.php
@@ -3,16 +3,28 @@
 namespace Action\Backend\Member;
 
 use Action\Backend\GenericLoggedInAction;
+use Config;
 use Doctrine\ORM\ORMException;
+use JSONObject;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 class Confirm extends GenericLoggedInAction {
 
+    const CONFIRMATION_EMAIL_SUBJECT = '';
+
+    const CONFIRMATION_EMAIL_TEXT = '';
+
+    const CONFIRMATION_EMAIL_HTML = [];
+
     /**
      * {@inheritdoc}
      * @throws ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
      */
     public function __invoke(Request $request, Response $response, array $args): ResponseInterface {
         /** @var \Domain\Member $member */
@@ -22,8 +34,25 @@ class Confirm extends GenericLoggedInAction {
             return $response->withRedirect('/admin/member/all?e=Member does exist');
         }
 
-        $member->setConfirmed(!$request->getQueryParam('deconfirm'));
+        $confirming = !$request->getQueryParam('deconfirm');
+
+        $member->setConfirmed(!$confirming);
         $this->em->flush();
+
+        if ($confirming) {
+            $this->send->email->transactional($member->getEmail(),
+                self::CONFIRMATION_EMAIL_SUBJECT,
+                self::CONFIRMATION_EMAIL_TEXT,
+                self::CONFIRMATION_EMAIL_HTML,
+                [
+                    'application' => $member->getId(),
+                    'name' => $member->getFirstName(),
+                    //TODO dynamic language (issue 10)
+                    'unionCopy' => JSONObject::get(Config::Pages, 'confirmationemail')['elements'][0]['fields']['display']['en'],
+                    'branchCopy' => JSONObject::get(Config::Branches, $member->getBranch())['welcome-text']['display']['en'],
+                ]
+            );
+        }
 
         return $response->withRedirect('/admin/member/' . $member->getId() . '?m=Member confirmation status changed&u=/admin/member/' . $member->getId() . '/confirm%3Fdeconfirm%3D' . ($member->isConfirmed() ? 1 : 0));
     }
