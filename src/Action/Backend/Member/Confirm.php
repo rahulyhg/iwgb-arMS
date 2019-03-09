0edc301ac77e971243f2e272d1c69703c6adfdee
Guy Mac <hello@guymac.eu>
2019-03-09 11:57:17 +0100

    Add confirmation email copy

--- src/Action/Backend/Member/Confirm.php
+++ src/Action/Backend/Member/Confirm.php
@@ -12,11 +12,29 @@ use Slim\Http\Response;
 
 class Confirm extends GenericLoggedInAction {
 
-    const CONFIRMATION_EMAIL_SUBJECT = '';
+    const CONFIRMATION_EMAIL_SUBJECT = 'Your IWGB membership is confirmed';
 
-    const CONFIRMATION_EMAIL_TEXT = '';
+    const CONFIRMATION_EMAIL_TEXT = "Application number: %application%\r\n\r\nHey %name,\r\nWe've just confirmed your application to the IWGB. We'll be in touch soon with your membership card.\r\n\r\n%copy%\r\n\r\n%branchcopy%\r\n\r\nIf you've got any further questions, please don't hesitate to give us a call. Hasta la victoria!\r\n\r\n— Your friends at the IWGB\r\n\r\nThis email was sent because you completed an application on iwgb.org.uk.\r\nIndependent Workers Union of Great Britain, 12-20 Baron St, London, N1 9LL";
 
-    const CONFIRMATION_EMAIL_HTML = [];
+    const CONFIRMATION_EMAIL_HTML= [
+        'content' => [
+            'before' => [
+                'Application number: %application%',
+                'Hey %name%,',
+                "We've just confirmed your application to the IWGB. We'll be in touch soon with your membership card.",
+            ],
+            'after' => [
+                '%copy%',
+                '%branchcopy%',
+                "If you've got any further questions, please don't hesitate to give us a call.",
+                'Hasta la victoria!',
+                '— Your friends at the IWGB',
+            ],
+            'footer' => [
+                'This email was sent because you completed an application on [iwgb.org.uk](https://iwgb.org.uk)',
+            ],
+        ],
+    ];
 
     /**
      * {@inheritdoc}
