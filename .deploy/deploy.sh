153c37d102eaa271e7245ad2001620d0fae00592
Guy Mac <hello@guymac.eu>
2019-03-11 01:29:11 +0000

    Cache dependencies

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -5,7 +5,7 @@ exec > /root/webhooks/iwgb-arms/output.log 2>&1
 git fetch --all
 git checkout --force "origin/master"
 
-rsync -a . /var/www/iwgb.org.uk --delete --exclude .git --exclude .db --exclude .deploy
+rsync -a . /var/www/iwgb.org.uk --delete --exclude .git --exclude .db --exclude .deploy --exclude vendor
 
 cd /var/repo/iwgb.org.uk-static
 rsync -a . /var/www/iwgb.org.uk
@@ -13,6 +13,7 @@ rsync -a . /var/www/iwgb.org.uk
 cd /var/www/iwgb.org.uk
 export COMPOSER_HOME=/usr/local/bin
 composer install
+composer update
 mkdir var
 mkdir var/doctrine
 mkdir var/upload
