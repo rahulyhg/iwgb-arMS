0ae756f92dc4022d365bd7b4919b111921dbb3e3
Guy Mac <hello@guymac.eu>
2019-03-11 00:03:45 +0000

    Add deploy script

---
+++ .deploy/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+exec > /root/webhooks/iwgb-arms/output.log 2>&1
+
+git fetch --all
+git checkout --force "origin/master"
+
+rsync -a . /var/www/iwgb.org.uk --delete --exclude .git --exclude .db --exclude .deploy
+
+cd /var/repo/iwgb.org.uk-static
+rsync -a . /var/www/iwgb.org.uk
+
+cd /var/www/iwgb.org.uk
+export COMPOSER_HOME=/usr/local/bin
+composer install
+mkdir var
+mkdir var/doctrine
+mkdir var/upload
+chmod -R 777 var
+
+cd public/css
+sass --update --no-cache --style compressed .:.
