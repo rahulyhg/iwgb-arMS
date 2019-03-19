bf3f65716d1e2b2e37fc45634c92a5bf0a69c8fb
Guy Mac <hello@guymac.eu>
2019-03-19 12:58:00 +0000

    Fixes #43

--- src/Action/Backend/Member/Confirm.php
+++ src/Action/Backend/Member/Confirm.php
@@ -5,6 +5,9 @@ namespace Action\Backend\Member;
 use Action\Backend\GenericLoggedInAction;
 use Config;
 use Doctrine\ORM\ORMException;
+use Domain\Event;
+use Domain\Member;
+use Domain\MemberRepository;
 use JSONObject;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Http\Request;
@@ -14,7 +17,7 @@ class Confirm extends GenericLoggedInAction {
 
     const CONFIRMATION_EMAIL_SUBJECT = 'Your IWGB membership is confirmed';
 
-    const CONFIRMATION_EMAIL_TEXT = "Application number: %application%\r\n\r\nHey %name,\r\nWe've just confirmed your application to the IWGB. We'll be in touch soon with your membership card.\r\n\r\n%copy%\r\n\r\n%branchcopy%\r\n\r\nIf you've got any further questions, please don't hesitate to give us a call. Hasta la victoria!\r\n\r\n— Your friends at the IWGB\r\n\r\nThis email was sent because you completed an application on iwgb.org.uk.\r\nIndependent Workers Union of Great Britain, 12-20 Baron St, London, N1 9LL";
+    const CONFIRMATION_EMAIL_TEXT = "Application number: %application%\r\n\r\nHey %name,\r\nWe've just confirmed your application to the IWGB. We'll be in touch soon with your membership card.\r\n\r\n%unionCopy%\r\n\r\n%branchCopy%\r\n\r\nIf you've got any further questions, please don't hesitate to give us a call. Hasta la victoria!\r\n\r\n— Your friends at the IWGB\r\n\r\nThis email was sent because you completed an application on iwgb.org.uk.\r\nIndependent Workers Union of Great Britain, 12-20 Baron St, London, N1 9LL";
 
     const CONFIRMATION_EMAIL_HTML= [
         'content' => [
@@ -24,8 +27,8 @@ class Confirm extends GenericLoggedInAction {
                 "We've just confirmed your application to the IWGB. We'll be in touch soon with your membership card.",
             ],
             'after' => [
-                '%copy%',
-                '%branchcopy%',
+                '%unionCopy%',
+                '%branchCopy%',
                 "If you've got any further questions, please don't hesitate to give us a call.",
                 'Hasta la victoria!',
                 '— Your friends at the IWGB',
@@ -43,18 +46,23 @@ class Confirm extends GenericLoggedInAction {
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
+     * @throws \Exception
      */
     public function __invoke(Request $request, Response $response, array $args): ResponseInterface {
-        /** @var \Domain\Member $member */
-        $member = $this->em->getRepository(\Domain\Member::class)->find($args['member']);
+        /** @var MemberRepository $memberRepo */
+        $memberRepo = $this->em->getRepository(Member::class);
+        /** @var Member $member */
+        $member = $memberRepo->find($args['member'], null, null, true);
 
         if (empty($member)) {
-            return $response->withRedirect('/admin/member/all?e=Member does exist');
+            return $response->withRedirect('/admin/member/all/0?e=Member does not exist');
         }
 
         $confirming = !$request->getQueryParam('deconfirm');
 
-        $member->setConfirmed(!$confirming);
+        $member->setConfirmed($confirming);
+
+
         $this->em->flush();
 
         if ($confirming) {
@@ -67,7 +75,7 @@ class Confirm extends GenericLoggedInAction {
                     'name' => $member->getFirstName(),
                     //TODO dynamic language (issue 10)
                     'unionCopy' => JSONObject::get(Config::Pages, 'confirmationemail')['elements'][0]['fields']['display']['en'],
-                    'branchCopy' => JSONObject::get(Config::Branches, $member->getBranch())['welcome-text']['display']['en'],
+                    'branchCopy' => JSONObject::get(Config::Branches, $member->getBranch())['welcome']['en'],
                 ]
             );
         }
